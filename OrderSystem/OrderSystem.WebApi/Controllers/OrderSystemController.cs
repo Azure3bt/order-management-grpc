b72527451cf0bc7811bf06bf02047bf90f1f8c58
Amir Sebt <amir.3bt@gmail.com>
2025-02-27 19:46:59 +0330

    Add api
    ; Conflicts:
    ;	OrderSystem/OrderSystem.WebApi/OrderSystem.WebApi.csproj
    ;	OrderSystem/OrderSystem.WebApi/Program.cs

---
+++ OrderSystem/OrderSystem.WebApi/Controllers/OrderSystemController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderSystem.SDK.Contract;
+using OrderSystem.SDK.RequestModel;
+
+namespace OrderSystem.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderSystemController : ControllerBase
+    {
+        private readonly ILogger<OrderSystemController> _logger;
+        private readonly IOrderService _orderService;
+        public OrderSystemController(ILogger<OrderSystemController> logger, IOrderService orderService)
+        {
+            _logger = logger;
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrders([FromQuery] GetOrderRequest request)
+        {
+            var response = await _orderService.GetAllOrder(request);
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddOrder([FromBody] CreateOrderRequest request)
+        {
+            var response = await _orderService.CreateOrder(request);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditOrder([FromBody] EditOrderRequest request)
+        {
+            var response = await _orderService.ModifyOrder(request);
+            if(response == null)
+            {
+                return NotFound();
+            }
+            return Ok(response);
+        }
+
+        [HttpDelete("{orderId}")]
+        public async Task<IActionResult> DeleteOrder(int orderId)
+        {
+            var response = await _orderService.DeleteOrder(orderId);
+            if (response == null)
+            {
+                return NotFound();
+            }
+            return Ok(response);
+        }
+    }
+}
