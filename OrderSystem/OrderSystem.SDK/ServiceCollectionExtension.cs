6a1aead80438050f2549973bc37eb5c63e2478ed
Amir Sebt <amir.3bt@gmail.com>
2025-02-27 20:34:03 +0330

    WOOPS - add usl for grpc service

--- OrderSystem/OrderSystem.SDK/ServiceCollectionExtension.cs
+++ OrderSystem/OrderSystem.SDK/ServiceCollectionExtension.cs
@@ -7,7 +7,9 @@ public static class ServiceCollectionExtension
 {
     public static void AddOrderServiceSdk(this IServiceCollection services)
     {
-        services.AddGrpcClient<OrderService.OrderServiceClient>();
+        services.AddGrpcClient<OrderService.OrderServiceClient>(option => {
+            option.Address = new Uri("https://localhost:7190");
+        });
         services.AddScoped<IOrderService, Implementation.OrderService>();
     }
 }
