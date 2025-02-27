f4aa74f4222ff668eeffd4c182a3aeca5e677248
Amir Sebt <amir.3bt@gmail.com>
2025-02-27 19:36:29 +0330

    Rename sdk registeration

--- OrderSystem/OrderSystem.SDK/ServiceCollectionExtension.cs
+++ OrderSystem/OrderSystem.SDK/ServiceCollectionExtension.cs
@@ -5,7 +5,7 @@ namespace OrderSystem.SDK;
 
 public static class ServiceCollectionExtension
 {
-    public static void AddSdk(this IServiceCollection services)
+    public static void AddOrderServiceSdk(this IServiceCollection services)
     {
         services.AddGrpcClient<OrderService.OrderServiceClient>();
         services.AddScoped<IOrderService, Implementation.OrderService>();
